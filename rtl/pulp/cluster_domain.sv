d6dbaaca5935908dae5583dd9d280f8e81ffebd5
Angelo Garofalo <angelo.garofalo@unibo.it>
2019-11-19 14:32:27 +0100

    add parameter for HPWE

--- rtl/pulp/cluster_domain.sv
+++ rtl/pulp/cluster_domain.sv
@@ -26,6 +26,7 @@ module cluster_domain
 #(
     //CLUSTER PARAMETERS
     parameter NB_CORES              = `NB_CORES,
+    parameter HWPE_PRESENT          = 0,
     parameter NB_HWPE_PORTS         = 4,
     parameter NB_DMAS               = 4,
 
@@ -253,6 +254,7 @@ module cluster_domain
 `ifndef USE_CLUSTER_NETLIST
     #(
         .NB_CORES                     ( NB_CORES                     ),
+        .HWPE_PRESENT                 ( HWPE_PRESENT                 ),
         .NB_HWPE_PORTS                ( NB_HWPE_PORTS                ),
         .NB_DMAS                      ( NB_DMAS                      ),
         .TCDM_SIZE                    ( TCDM_SIZE                    ),
