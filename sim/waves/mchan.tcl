4b5db19eb4a8c42ac2a7847103cb9ebace5d6bdf
Davide Rossi <davide.rossi@unibo.it>
2020-05-15 17:13:39 +0200

    updated ,mchan waves

---
+++ sim/waves/mchan.tcl
@@ -0,0 +1,88 @@
+add wave -radix hexadecimal -group "mchan" -group "top" -group "top" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/*
+add wave -radix hexadecimal -group "mchan" -group "top" -group "ctrl_slave_0" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/cl_ctrl_slave/*
+add wave -radix hexadecimal -group "mchan" -group "top" -group "ctrl_slave_1" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/fc_ctrl_slave/*
+add wave -radix hexadecimal -group "mchan" -group "top" -group "ctrl_slave_2" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/pe_ctrl_slave\[0\]/*
+add wave -radix hexadecimal -group "mchan" -group "top" -group "ctrl_slave_3" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/pe_ctrl_slave\[1\]/*
+add wave -radix hexadecimal -group "mchan" -group "top" -group "ctrl_slave_4" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/pe_ctrl_slave\[2\]/*
+add wave -radix hexadecimal -group "mchan" -group "top" -group "ctrl_slave_5" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/pe_ctrl_slave\[3\]/*
+add wave -radix hexadecimal -group "mchan" -group "top" -group "ctrl_slave_6" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/pe_ctrl_slave\[4\]/*
+add wave -radix hexadecimal -group "mchan" -group "top" -group "ctrl_slave_7" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/pe_ctrl_slave\[5\]/*
+add wave -radix hexadecimal -group "mchan" -group "top" -group "ctrl_slave_8" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/pe_ctrl_slave\[6\]/*
+add wave -radix hexadecimal -group "mchan" -group "top" -group "ctrl_slave_9" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/pe_ctrl_slave\[7\]/*
+add wave -radix hexadecimal -group "mchan" -group "top" -group "ext_master" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/ext_master/*
+add wave -radix hexadecimal -group "mchan" -group "top" -group "tcdm_master_0" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/tcdm_master\[0\]/*
+add wave -radix hexadecimal -group "mchan" -group "top" -group "tcdm_master_1" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/tcdm_master\[1\]/*
+add wave -radix hexadecimal -group "mchan" -group "top" -group "tcdm_master_2" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/tcdm_master\[2\]/*
+add wave -radix hexadecimal -group "mchan" -group "top" -group "tcdm_master_3" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/tcdm_master\[3\]/*
+
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_unit_top" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_if" -group "ctrl_if_0" -group "ctrl_if_0_top"     /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/ctrl_if_bind\[0\]/ctrl_if_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_if" -group "ctrl_if_0" -group "ctrl_fsm_0" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/ctrl_if_bind\[0\]/ctrl_if_i/ctrl_fsm_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_if" -group "ctrl_if_1" -group "ctrl_if_1_top"     /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/ctrl_if_bind\[1\]/ctrl_if_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_if" -group "ctrl_if_1" -group "ctrl_fsm_1" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/ctrl_if_bind\[1\]/ctrl_if_i/ctrl_fsm_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_if" -group "ctrl_if_2" -group "ctrl_if_2_top"     /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/ctrl_if_bind\[2\]/ctrl_if_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_if" -group "ctrl_if_2" -group "ctrl_fsm_2" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/ctrl_if_bind\[2\]/ctrl_if_i/ctrl_fsm_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_if" -group "ctrl_if_3" -group "ctrl_if_3_top"     /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/ctrl_if_bind\[1\]/ctrl_if_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_if" -group "ctrl_if_3" -group "ctrl_fsm_3" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/ctrl_if_bind\[3\]/ctrl_if_i/ctrl_fsm_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_if" -group "ctrl_if_4" -group "ctrl_if_4_top"     /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/ctrl_if_bind\[1\]/ctrl_if_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_if" -group "ctrl_if_4" -group "ctrl_fsm_4" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/ctrl_if_bind\[4\]/ctrl_if_i/ctrl_fsm_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_if" -group "ctrl_if_5" -group "ctrl_if_5_top"     /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/ctrl_if_bind\[1\]/ctrl_if_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_if" -group "ctrl_if_5" -group "ctrl_fsm_5" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/ctrl_if_bind\[5\]/ctrl_if_i/ctrl_fsm_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_if" -group "ctrl_if_6" -group "ctrl_if_6_top"     /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/ctrl_if_bind\[1\]/ctrl_if_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_if" -group "ctrl_if_6" -group "ctrl_fsm_6" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/ctrl_if_bind\[6\]/ctrl_if_i/ctrl_fsm_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_if" -group "ctrl_if_7" -group "ctrl_if_7_top"     /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/ctrl_if_bind\[1\]/ctrl_if_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_if" -group "ctrl_if_7" -group "ctrl_fsm_7" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/ctrl_if_bind\[7\]/ctrl_if_i/ctrl_fsm_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_if" -group "ctrl_if_8" -group "ctrl_if_8_top"     /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/ctrl_if_bind\[1\]/ctrl_if_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_if" -group "ctrl_if_8" -group "ctrl_fsm_8" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/ctrl_if_bind\[8\]/ctrl_if_i/ctrl_fsm_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_if" -group "ctrl_if_9" -group "ctrl_if_9_top"     /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/ctrl_if_bind\[1\]/ctrl_if_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "ctrl_if" -group "ctrl_if_9" -group "ctrl_fsm_9" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/ctrl_if_bind\[9\]/ctrl_if_i/ctrl_fsm_i/*
+
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "synch_unit" -group "synch_unit_0" -group "synch_unit_0_top"     /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/synch_unit_bind\[0\]/synch_unit_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "synch_unit" -group "synch_unit_1" -group "synch_unit_1_top"     /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/synch_unit_bind\[1\]/synch_unit_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "synch_unit" -group "synch_unit_2" -group "synch_unit_2_top"     /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/synch_unit_bind\[2\]/synch_unit_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "synch_unit" -group "synch_unit_3" -group "synch_unit_3_top"     /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/synch_unit_bind\[3\]/synch_unit_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "synch_unit" -group "synch_unit_4" -group "synch_unit_4_top"     /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/synch_unit_bind\[4\]/synch_unit_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "synch_unit" -group "synch_unit_5" -group "synch_unit_5_top"     /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/synch_unit_bind\[5\]/synch_unit_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "synch_unit" -group "synch_unit_6" -group "synch_unit_6_top"     /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/synch_unit_bind\[6\]/synch_unit_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "synch_unit" -group "synch_unit_7" -group "synch_unit_7_top"     /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/synch_unit_bind\[7\]/synch_unit_i/*
+
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "trans_allocator" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/trans_allocator_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "trans_arbiter" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/trans_arbiter_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "trans_queue" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/trans_queue_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "twd_trans_queue" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/twd_ext_trans_queue_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "twd_trans_queue" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/twd_tcdm_trans_queue_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "tx_twd_trans_splitter" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/tx_twd_trans_splitter_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "rx_twd_trans_splitter" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/rx_twd_trans_splitter_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "tx_trans_queue" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/tx_trans_queue_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "rx_trans_queue" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/rx_trans_queue_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "tx_trans_unpack" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/tx_trans_unpack_i/*
+add wave -radix hexadecimal -group "mchan" -group "ctrl_unit" -group "rx_trans_unpack" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ctrl_unit_i/rx_trans_unpack_i/*
+
+add wave -radix hexadecimal -group "mchan" -group "trans_unit" -group "trans_unit_top" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/trans_unit_i/*
+add wave -radix hexadecimal -group "mchan" -group "trans_unit" -group "trans_buffers_tcdm" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/trans_unit_i/trans_buffers_tcdm_i/*
+add wave -radix hexadecimal -group "mchan" -group "trans_unit" -group "trans_buffer_ext_tx" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/trans_unit_i/trans_buffer_ext_tx_i/*
+add wave -radix hexadecimal -group "mchan" -group "trans_unit" -group "trans_buffer_ext_rx" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/trans_unit_i/trans_buffer_ext_rx_i/*
+add wave -radix hexadecimal -group "mchan" -group "trans_unit" -group "trans_queue_tx" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/trans_unit_i/trans_queue_tx_i/*
+add wave -radix hexadecimal -group "mchan" -group "trans_unit" -group "trans_queue_rx" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/trans_unit_i/trans_queue_rx_i/*
+add wave -radix hexadecimal -group "mchan" -group "trans_unit" -group "trans_aligner_tx" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/trans_unit_i/trans_aligner_tx_i/*
+add wave -radix hexadecimal -group "mchan" -group "trans_unit" -group "trans_aligner_rx" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/trans_unit_i/trans_aligner_rx_i/*
+
+add wave -radix hexadecimal -group "mchan" -group "tcdm_unit" -group "tcdm_unit_top"  /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/tcdm_unit_i/*
+add wave -radix hexadecimal -group "mchan" -group "tcdm_unit" -group "tcdm_tx_cmd_queue"    /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/tcdm_unit_i/tcdm_tx_cmd_queue_i/*
+add wave -radix hexadecimal -group "mchan" -group "tcdm_unit" -group "tcdm_rx_cmd_queue"    /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/tcdm_unit_i/tcdm_rx_cmd_queue_i/*
+add wave -radix hexadecimal -group "mchan" -group "tcdm_unit" -group "tcdm_tx_cmd_unpack"   /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/tcdm_unit_i/tcdm_tx_cmd_unpack_i/*
+add wave -radix hexadecimal -group "mchan" -group "tcdm_unit" -group "tcdm_rx_cmd_unpack"   /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/tcdm_unit_i/tcdm_rx_cmd_unpack_i/*
+add wave -radix hexadecimal -group "mchan" -group "tcdm_unit" -group "tcdm_tx_cmd_unpack_0" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/tcdm_unit_i/beat_queue_tx\[0\]/tcdm_beat_queue_tx_i/*
+add wave -radix hexadecimal -group "mchan" -group "tcdm_unit" -group "tcdm_tx_cmd_unpack_1" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/tcdm_unit_i/beat_queue_tx\[1\]/tcdm_beat_queue_tx_i/*
+add wave -radix hexadecimal -group "mchan" -group "tcdm_unit" -group "tcdm_rx_cmd_unpack_0" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/tcdm_unit_i/beat_queue_rx\[0\]/tcdm_beat_queue_rx_i/*
+add wave -radix hexadecimal -group "mchan" -group "tcdm_unit" -group "tcdm_rx_cmd_unpack_1" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/tcdm_unit_i/beat_queue_rx\[1\]/tcdm_beat_queue_rx_i/*
+add wave -radix hexadecimal -group "mchan" -group "tcdm_unit" -group "tcdm_tx_if_0"         /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/tcdm_unit_i/tcdm_if_tx\[0\]/tcdm_if_tx_i/*
+add wave -radix hexadecimal -group "mchan" -group "tcdm_unit" -group "tcdm_tx_if_1"         /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/tcdm_unit_i/tcdm_if_tx\[1\]/tcdm_if_tx_i/*
+add wave -radix hexadecimal -group "mchan" -group "tcdm_unit" -group "tcdm_rx_if_0"         /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/tcdm_unit_i/tcdm_if_rx\[0\]/tcdm_if_rx_i/*
+add wave -radix hexadecimal -group "mchan" -group "tcdm_unit" -group "tcdm_rx_if_1"         /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/tcdm_unit_i/tcdm_if_rx\[1\]/tcdm_if_rx_i/*
+add wave -radix hexadecimal -group "mchan" -group "tcdm_unit" -group "tcdm_synch_tx"        /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/tcdm_unit_i/tcdm_synch_tx_i/*
+add wave -radix hexadecimal -group "mchan" -group "tcdm_unit" -group "tcdm_synch_rx"        /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/tcdm_unit_i/tcdm_synch_rx_i/*
+
+add wave -radix hexadecimal -group "mchan" -group "ext_unit" -group "ext_unit_top" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ext_unit_i/*
+add wave -radix hexadecimal -group "mchan" -group "ext_unit" -group "ext_rx_if" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ext_unit_i/ext_rx_if_i/*
+add wave -radix hexadecimal -group "mchan" -group "ext_unit" -group "ext_tx_if" /tb_pulp/i_dut/cluster_domain_i/cluster_i/dmac_wrap_i/mchan_i/ext_unit_i/ext_tx_if_i/*
